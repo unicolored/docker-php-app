72ce9f8e248d4105d5f2ae70c74a3eca04f9a9cb
Gilles Hoarau <unicolored@gmail.com>
2025-02-22 08:55:30 +0100

    Add build script for Docker image deployment
    Introduce `build.sh` to automate Docker image building and pushing using `buildx`. Adjust `.gitignore` to allow version control of `.sh` scripts while keeping sensitive files excluded.

---
+++ build.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+set -e
+
+GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+REPOSITORY=php-app
+TAG=$GIT_BRANCH
+DOCKER_HOST=unicolored
+DOCKER_REPO="${DOCKER_HOST}/${REPOSITORY}:${TAG}"
+DOCKER_REPO_LATEST="${DOCKER_HOST}/${REPOSITORY}:latest"
+
+docker buildx build --builder cloud-unicolored-my-cloud-builder --push -t "${DOCKER_REPO}" -t "${DOCKER_REPO_LATEST}" .
+
+docker buildx build -t "${DOCKER_REPO}" -t "${DOCKER_REPO_LATEST}" .
+docker push "${DOCKER_REPO}"
+docker push "${DOCKER_REPO_LATEST}"
